0a4ddd91b6f23d358e130b7ba0b61d72063c9276
Denis Fomintsev <90705279+DeFomin@users.noreply.github.com>
2021-11-03 13:43:59 +0300

    Update 6.py

--- u4/6.py
+++ u4/6.py
@@ -1,12 +1,13 @@
-print('Вводите через пробел:')
+print('Вводите: ')
 a = []
 count = 0
-prov = '0123456789'
+
 while True:
     n = input()
-    if (n.isdigit() == 1):
+
+    if (str(n).lstrip('-').replace('.', '', 1).isdigit() == 1):
         a.append(n)
         count += 1
     else:
+        print(count, a)
         break
-print(count, a)
